e2fa0fc9532f836f766a5b3b36df04b28b72d4de
danielg <danielg@sqreamtech.com>
2023-02-26 17:09:29 +0200

    add files

---
+++ plugins/SQreamDB/connectionProperties.js
@@ -0,0 +1,21 @@
+(function propertiesbuilder(attr) {
+    var props = {};
+    var urlBuilder = "jdbc:Sqream://";
+
+    urlBuilder = urlBuilder.concat(attr[connectionHelper.attributeServer] + ":" + attr[connectionHelper.attributePort] + "/" + attr[connectionHelper.attributeDatabase]);
+    props["user"] = attr[connectionHelper.attributeUsername];
+    props["password"] = attr[connectionHelper.attributePassword];
+    props["ssl"] = "false";
+    props["cluster"] = attr["warehouse"];
+
+    if (attr[connectionHelper.attributeSSLMode] == "require") {
+        props["ssl"] = "true";
+    }
+
+    for (var key in props)
+    {
+        urlBuilder = urlBuilder.concat(";" + key + "=" + props[key]);
+    }
+
+    return [urlBuilder];
+})
